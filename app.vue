461aa3cf9aad4192c0dba76f8bd7f25e789963c3
Cyan Changes <contact@cyans.me>
2025-01-06 21:12:29 +0800

    chore: add copyright notice

--- app.vue
+++ app.vue
@@ -1,9 +1,35 @@
 <template>
   <Toast/>
   <NuxtRouteAnnouncer/>
-  <NuxtPage/>
+  <main>
+    <NuxtPage/>
+  </main>
+  <footer class="w-full bottom-0 text-center text-bold">
+    <p>
+      <divider>Made For <span class="t-oac">Onlyacat233</span></divider>
+      Copyright &copy; 2025-present <span class="t-cyan">Cyan Changes</span>
+    </p>
+  </footer>
 </template>
 
 <script>
 import 'primeicons/primeicons.css'
+
+useHead({
+  title: "Random Fortune - Oac"
+})
 </script>
+
+<style lang="scss">
+.t-cyan {
+  background: linear-gradient(25deg, #2dceff, #2dffd8);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: 1000;
+}
+
+.t-oac {
+  font-weight: 1000;
+  color: fuchsia;
+}
+</style>
